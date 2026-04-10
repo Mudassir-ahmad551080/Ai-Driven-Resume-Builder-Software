5f2d999ffbcb60b0d280e8be07fe1d984324cbfd
Mudassir Ahmad <ma6386731@gmail.com>
2026-04-10 14:36:50 +0500

    anohter commit

--- backend/src/app/app.js
+++ backend/src/app/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 const corsOptions = {
   origin: [
-    'http://localhost:5173',
+    'http://localhost:5176',
     'https://resume-frontend-psi-one.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
